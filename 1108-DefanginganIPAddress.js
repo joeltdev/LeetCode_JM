e7b24f0853de1fdba7ebc3b50c90014892b1d86a
Joel Thomas Mathew <joelmathew857@gmail.com>
2024-04-30 09:08:33 +0530

    lc-1108

--- 1108-DefanginganIPAddress.js
+++ 1108-DefanginganIPAddress.js
@@ -1,5 +1,6 @@
-let array = [1, 2, 3, 2, 3, 1, 2, 3];
+var defangIPaddr = function (address) {
+  return address.split(".").join("[.]");
+};
 
-let replacedArray = array.map((num) => (num === 1 ? 2 : num));
-
-console.log(replacedArray); // Output: [2, 2, 3, 2, 3, 2, 2, 3]
+// Example usage:
+console.log(defangIPaddr("192.168.1.1")); // Output: "192[.]168[.]1[.]1"
